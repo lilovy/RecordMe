c3cee2039971001a60bccb64112ac37bbb200454
lilovy <43091143+lilovy@users.noreply.github.com>
2024-01-11 22:53:12 +0500

    first strokes

--- app/src/main/java/com/lilovy/recordme/MainActivity.kt
+++ app/src/main/java/com/lilovy/recordme/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.lilovy.recordme
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.core.app.ActivityCompat
+import java.io.File
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
 }
---
+++ app/src/main/java/com/lilovy/recordme/RecorderWaveformView.kt
@@ -0,0 +1,16 @@
+package com.lilovy.recordme
+
+
+import android.content.Context
+import android.view.View
+
+class RecorderWaveformView(context: Context?) : View(context) {
+
+    init {
+        init()
+    }
+
+    private fun init() {
+
+    }
+}
